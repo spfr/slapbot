3dcba34043326c593c34b9c1387d07ec6fcccac7
Miloš Žikić <milos@spicefactory.co>
2015-08-04 23:28:33 +0200

    incoming webhook

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // grab the packages we need
 var express = require('express');
+var request = require('request');
 var app = express();
 var port = process.env.PORT || 8080;
 
@@ -17,10 +18,48 @@ app.post('/slapbot', function(req, res) {
 	if (shouldslap == undefined) {
 		res.status(400).send('Need a user to slap! DUUUH!');
 	} else {
-	
-		res.send(username + " slaps " + shouldslap + " around a bit with a large trout! :fish: ");
+
+		
+		var botPayload = {
+			"text": username + " slaps " + shouldslap + " around a bit with a large trout! :fish: ",
+			"username" : "slapbot",
+			"channel" : req.body.channel_id,
+			"icon_emoji" : ":fish:"
+
+		};
+
+		send(botPayload, function (error, status, body) {
+			if (error) {
+				return next(error);
+
+			} else if (status !== 200) {
+		      // inform user that our Incoming WebHook failed
+		      return next(new Error('Incoming WebHook: ' + status + ' ' + body));
+
+			  } else {
+			  	res.send("Yes Sir, Let me slap "+  shouldslap + " around a bit with a large trout! :fish: ");
+			  }
+			});
+
 	}
 });
+
+function send (payload, callback) {
+	var path = process.env.INCOMING_WEBHOOK_PATH;
+	var uri = 'https://hooks.slack.com/services' + path;
+
+	request({
+		uri: uri,
+		method: 'POST',
+		body: JSON.stringify(payload)
+	}, function (error, response, body) {
+		if (error) {
+			return callback(error);
+		}
+
+		callback(null, response.statusCode, body);
+	});
+}
 
 // start the server
 app.listen(port);
